0b95cd39340d20c11864ad3721338005765b774c
Iswinsyah <iswinsyah@example.com>
2026-04-04 07:39:04 +0700

    Update: Mengubah favicon tab browser dengan logo At Tahfidz

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+// Mengubah Favicon (Logo Tab Browser) secara otomatis
+let faviconLink = document.querySelector("link[rel~='icon']");
+if (!faviconLink) {
+  faviconLink = document.createElement('link');
+  faviconLink.rel = 'icon';
+  document.head.appendChild(faviconLink);
+}
+faviconLink.href = 'https://raw.githubusercontent.com/iswinsyah/Gambar/refs/heads/main/logo%20Tahfidz.jfif';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
